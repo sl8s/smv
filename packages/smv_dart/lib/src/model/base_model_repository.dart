17756fe8a189c0293901058d0c5bcbe5e026c67a
sl8s <sl8sqw@gmail.com>
2026-08-05 14:59:17 +0300

    feature(packages/smv_dart/): Added methods 'fromMap','fromListMap' and jenerics 'BaseModel', 'BaseListModel' in the class 'BaseModelRepository' and added jenerics 'BaseModel', 'BaseListModel' in the class 'ResultListModel'

--- packages/smv_dart/lib/src/model/base_model_repository.dart
+++ packages/smv_dart/lib/src/model/base_model_repository.dart
@@ -1,10 +1,18 @@
 import 'package:meta/meta.dart';
 import 'package:smv_dart/src/idispose.dart';
+import 'package:smv_dart/src/model/base_model.dart';
+import 'package:smv_dart/src/model/base_list_model.dart';
 
 @immutable
-abstract base class BaseModelRepository implements IDispose {
+abstract base class BaseModelRepository<Y extends BaseModel, U extends BaseListModel<Y>> implements IDispose {
   const BaseModelRepository();
-
+  
+  @protected
+  Y fromMap(Map<String, dynamic> map);
+  
+  @protected
+  U fromListMap(List<Map<String, dynamic>> listMap);
+  
   @protected
   @nonVirtual
   T getSafeValue<T>(Map<String, dynamic> map, String key, T defaultValue) {
--- packages/smv_dart/lib/src/result_list_model.dart
+++ packages/smv_dart/lib/src/result_list_model.dart
@@ -1,11 +1,12 @@
 import 'package:meta/meta.dart';
 import 'package:smv_dart/src/base_exception.dart';
+import 'package:smv_dart/src/model/base_model.dart';
 import 'package:smv_dart/src/model/base_list_model.dart';
 import 'package:smv_dart/src/exception_adapter.dart';
 
 @immutable
-final class ResultListModel<T extends BaseListModel> {
-  final T? data;
+final class ResultListModel<T extends BaseModel, Y extends BaseListModel<T>> {
+  final Y? data;
   final ExceptionAdapter exceptionAdapter;
 
   ResultListModel.success(this.data)
